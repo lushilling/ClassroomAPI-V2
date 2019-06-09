152bb941436544d9c6d4f086ca89c5f9240a44eb
lucy <lshilling@hotmail.co.uk>
2019-06-09 16:40:40 +0100

    added a retrieve method to the classroom and trainee repository

--- src/main/java/com/qa/persistance/Repository/TraineeDatabase.java
+++ src/main/java/com/qa/persistance/Repository/TraineeDatabase.java
@@ -1,9 +1,13 @@
 package com.qa.persistance.Repository;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.transaction.Transactional.TxType;
+
+import com.qa.Business.Trainee;
+import com.qa.Utility.JSONUtil;
 
 @Transactional(value = TxType.SUPPORTS)
 public class TraineeDatabase implements TraineeRepository {
@@ -11,11 +15,18 @@ public class TraineeDatabase implements TraineeRepository {
 	@PersistenceContext(unitName = "myPu")
 	private EntityManager manager;
 
+	@Inject
+	private JSONUtil util;
+
 	@Transactional(value = TxType.REQUIRED)
 	public String add(String trainee) {
 		manager.persist(trainee);
 		return trainee;
-		
+
+	}
+
+	public String retrieve(int id) {
+		return util.getJSONForObject(manager.find(Trainee.class, id));
 	}
 
 }
